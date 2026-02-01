05da14a2b6f71fe07d315e2422d27b5463317efe
officialcbk <eeze@academic.rrc.ca>
2026-02-01 17:54:39 -0600

    Added the organization file

--- labs/src/App.tsx
+++ labs/src/App.tsx
@@ -2,6 +2,8 @@ import './App.css'
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './main';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 
 const App = () => {
   return (
