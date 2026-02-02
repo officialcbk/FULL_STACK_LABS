ff9a09d6b5b22e815bb20c5e12d50339d4a174e8
officialcbk <eeze@academic.rrc.ca>
2026-02-01 18:00:55 -0600

    Added the layout component

---
+++ labs/src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
+import Navbar from './Navbar';
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
