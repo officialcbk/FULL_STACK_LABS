505d53f21cdd0adfa8910ab6c0fabed6b2d05219
officialcbk <eeze@academic.rrc.ca>
2026-02-01 18:08:59 -0600

    Updated app.tsx

--- labs/src/App.tsx
+++ labs/src/App.tsx
@@ -3,15 +3,21 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './main';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-
+import Layout from './components/Layout';
+import Employees from './Employees';
+import Organization from './Organization';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Main/>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/employees" replace />} />
+          <Route path="employees" element={<Employees />} />
+          <Route path="organization" element={<Organization />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
